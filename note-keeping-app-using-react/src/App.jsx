a467d2986062074df3a70cddb3d244a0f6de11f6
Prasanna V <you@example.com>
2024-01-31 11:52:39 +0530

    coded

---
+++ note-keeping-app-using-react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import EditorComponent from './components/Editor';
+
+function App() {
+  return (
+    <div className="App">
+      <EditorComponent />
+    </div>
+  );
+}
+
+export default App;
